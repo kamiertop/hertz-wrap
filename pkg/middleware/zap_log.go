eb932c3300ab2ad5cc0aab8aff272211afef20f1
XiaoLiang <jxl762610973@gmail.com>
2024-02-14 18:55:48 +0800

    :sparkles: Add log middleware.

--- pkg/middleware/zap_log.go
+++ pkg/middleware/zap_log.go
@@ -2,12 +2,37 @@ package middleware
 
 import (
 	"context"
+	"time"
 
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"go.uber.org/zap"
+
+	"hertz/pkg/log"
+	"hertz/pkg/utils"
 )
 
-func Logger() app.HandlerFunc {
-	return func(_ context.Context, ctx *app.RequestContext) {
+const _execTimeout = 1
 
+// Logger middleware record request and response information.
+func Logger() app.HandlerFunc {
+	return func(c context.Context, ctx *app.RequestContext) {
+		logMsg := []zap.Field{
+			zap.String("ip", ctx.RemoteAddr().String()),
+			zap.String("agent", utils.BytesToString(ctx.UserAgent())),
+		}
+		s := time.Now()
+		ctx.Next(c)
+		cost := time.Since(s)
+		logMsg = append(logMsg, zap.String("cost", cost.String()))
+		if ctx.Response.StatusCode() == consts.StatusOK {
+			if cost.Seconds() > _execTimeout {
+				log.Warn(ctx.Request.URI().String(), logMsg...)
+			} else {
+				log.Info(ctx.Request.URI().String(), logMsg...)
+			}
+		} else {
+			log.Error(ctx.Request.URI().String(), logMsg...)
+		}
 	}
 }
