82e6dde23f746b8b2135497dc051d04ff2591d65
XiaoLiang <jxl762610973@gmail.com>
2024-02-26 10:10:48 +0800

    :sparkles: Add lookUp in key_auth middleware to avoid panic.

--- pkg/middleware/key_auth.go
+++ pkg/middleware/key_auth.go
@@ -7,5 +7,12 @@ import (
 
 // KeyAuth wrap from hertz
 func KeyAuth() app.HandlerFunc {
-	return keyauth.New()
+	return keyauth.New(
+		keyauth.WithKeyLookUp(lookUp("header", "Authorization"), "Bearer"),
+	)
+}
+
+// lookUp set key auth lookUp, panic can be avoided in keyauth.WithKeyLookUp.
+func lookUp(key, value string) string {
+	return key + ":" + value
 }
