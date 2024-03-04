6656db1b1fb148235c41f5fc3ac64f108033670a
XiaoLiang <jxl762610973@gmail.com>
2024-03-04 22:16:16 +0800

    ✨ Add keyAuth middleware.

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 - go version: `go1.22.0`
 - 项目重命名: 可以使用Goland快速重命名项目
 
+# database
+- sqlc: 使用sqlc生成
+
 ### 中间件
 - zap-log: 非常细致的日志记录
 - basic_auth: 使用Gin的basic_auth, 并做了一些修改(Hertz的basic_auth有一点点小问题)
--- pkg/middleware/key_auth.go
+++ pkg/middleware/key_auth.go
@@ -1,14 +1,36 @@
 package middleware
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/hertz-contrib/keyauth"
 )
 
-// KeyAuth wrap from hertz
+// KeyAuth wrap from hertz, you can set it manually.
 func KeyAuth() app.HandlerFunc {
 	return keyauth.New(
+		keyauth.WithContextKey("token"),
 		keyauth.WithKeyLookUp(lookUp("header", "Authorization"), "Bearer"),
+		// The middleware is skipped when true is returned.
+		keyauth.WithFilter(func(c context.Context, ctx *app.RequestContext) bool {
+			return true
+		}),
+
+		// It may be used to validate key.
+		// If returns false or err != nil, then errorHandler is used.
+		keyauth.WithValidator(func(ctx context.Context, requestContext *app.RequestContext, s string) (bool, error) {
+			return false, keyauth.ErrMissingOrMalformedAPIKey
+		}),
+
+		// It may be used to define a custom error.
+		keyauth.WithErrorHandler(func(ctx context.Context, requestContext *app.RequestContext, err error) {
+			requestContext.AbortWithMsg("msg", http.StatusBadRequest)
+		}),
+		keyauth.WithSuccessHandler(func(c context.Context, ctx *app.RequestContext) {
+			ctx.Next(c)
+		}),
 	)
 }
 
